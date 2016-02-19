fd322b17878893b4cce0456cf525eeb6bdb257e6
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2016-02-19 10:36:01 -0300

    Classe TLoggerHTML

---
+++ app.ado/TLoggerHTML.class.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	class TLoggerHTML extends TLogger{
+
+		public function write($message){
+
+			$time  = date("Y-m-d H:i:s");
+			$text  = "<p>\n";
+			$text .= "	<b>$time</b> : \n";
+			$text .= "	<i>$message</i> <br> \n";
+			$text .= "</p> \n";
+
+			$handler = fopen(this->filename, 'a');
+			fwrite($handler, $text);
+			fclose($handler);
+
+		}
+
+	}
+
+ ?>
