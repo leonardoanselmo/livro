8725d7912098239676c0f04ba7847ce91726aab7
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2016-02-19 08:50:53 -0300

    Classe TLogger abstrata

---
+++ app.ado/TLogger.class.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	abstract class TLogger{
+
+		protected $filename;
+
+		public function __construct($filename){
+			$this->filename = $filename;
+			file_put_contents($filename, '');
+		}
+
+		abstract function write($message);
+		
+	}
+
+ ?>
