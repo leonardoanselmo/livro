1a1e940b2079cfbd16d3615270645f139417231b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2016-02-24 11:10:12 -0300

    Exemplo de domain model 2

---
+++ domain_model2.php
@@ -0,0 +1,74 @@
+<?php 
+	
+	final class Produto{
+
+		private $descricao;   // descricao do produto
+		private $estoque;     // estoque do produto
+		private $preco_custo; // preco de custo 
+
+		public function __construct($descricao, $estoque, $preco_custo){
+			$this->descricao   = $descricao;
+			$this->estoque     = $estoque;
+			$this->preco_custo = $preco_custo;
+		}
+
+		public function registraCompra($unidades, $preco_custo){
+			$this->preco_custo = $preco_custo;
+			$this->estoque    += $unidades;
+		}
+
+		public function registraVenda($unidades){
+			$this->estoque -= $unidades;
+		}
+
+		public function getEstoque(){
+			return $this->estoque;
+		}
+
+		public function calculaPrecoVenda(){
+			return $this->preco_custo * 1.3;
+		}
+
+	}
+
+	final class Venda{
+
+		private $itens;
+
+		public function addItem($quantidade, Produto $produto){
+			$this->itens[] = array($quantidade, $produto);
+		}
+
+		public function getItens(){
+			return $this->itens;
+		}
+
+		public function finalizaVenda(){
+
+			$total = 0;
+
+			foreach ($this->itens as $item) {
+
+				$quantidade = $item[0];
+				$produto    = $item[1];
+
+				$total += $produto->calculaPrecoVenda() * $quantidade;
+				$produto->registraVenda($quantidade);
+			}
+
+			return $total;
+		}
+
+	}
+
+	// Instanciando objeto Venda
+
+	$venda = new Venda;
+
+	$venda->addItem(3, new Produto('Vinho', 10, 15));
+	$venda->addItem(2, new Produto('Salame', 20, 20));
+	$venda->addItem(1, new Produto('Queijo', 30, 10));
+
+	echo $venda->finalizaVenda();	
+
+ ?>
