37a5707ef7893b9171659c548852db906853dcdc
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2016-03-07 15:38:28 -0300

    Exemplo de exibicao de imagens pela classe TImage

---
+++ images.php
@@ -0,0 +1,12 @@
+<?php 
+
+	include_once "app.widgets/TElement.class.php";
+	include_once "app.widgets/TImage.class.php";
+
+	$gnome = new TImage('app.images/gnome.png');
+	$gnome->show();
+
+	$pinguim = new TImage('app.images/pinguim.jpg');
+	$pinguim->show();
+	
+ ?>
