a8ef2098717b37b73ac8fecfbaa7e65a43045ce0
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2016-03-07 15:51:42 -0300

    Classe TParagraph

---
+++ app.widgets/TParagraph.class.php
@@ -0,0 +1,16 @@
+<?php 
+	
+	class TParagraph extends TElement{
+
+		public function __construct($text){
+			parent::__construct('p');
+			parent::add($text);
+		}
+
+		function setAlign($align){
+			$this->align = $align;
+		}
+
+	}
+
+ ?>
